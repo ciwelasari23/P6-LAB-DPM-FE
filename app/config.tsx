6e39cbdb699e6a7c9c91714c56b044c42f58272c
ciwelasari23 <ciciwelasari23dz@gmail.com>
2024-12-22 21:47:49 +0700

    first commit

---
+++ app/config.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ConfigScreen() {
+  return (
+    <View>
+      <Text>API URL: http:// 192.168.210.111:8000</Text>
+    </View>
+  );
+}
